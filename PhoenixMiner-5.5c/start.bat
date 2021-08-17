1000095bfd98f10d27cca3310ee06c49d5c4b261
vihagad <88770001+vihagad@users.noreply.github.com>
2021-08-18 00:03:00 +0530

    Add files via upload

---
+++ PhoenixMiner-5.5c/start.bat
@@ -0,0 +1,3 @@
+@echo off
+PhoenixMiner.exe -pool ssl://us1-etc.ethermine.org:5555 -pool2 ssl://eu1-etc.ethermine.org:5555 -ewal 0x6ff85749ffac2d3a36efa2bc916305433fa93731.bd7tlmbhnbxukcy -gpow 96
+pause
